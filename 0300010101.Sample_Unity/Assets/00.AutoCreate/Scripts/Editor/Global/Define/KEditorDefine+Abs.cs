b6273b37e3620aaea2b1559e602fe496748627a2
sd.lee <sd.lee@ninetap.com>
2022-05-25 08:17:03 +0900

    [이상동] 서브 모듈 갱신

--- 0300010101.Sample_Unity/Assets/00.AutoCreate/Scripts/Editor/Global/Define/KEditorDefine+Abs.cs
+++ 0300010101.Sample_Unity/Assets/00.AutoCreate/Scripts/Editor/Global/Define/KEditorDefine+Abs.cs
@@ -162,6 +162,7 @@ public static partial class KEditorDefine {
 		// 기본
 		["com.unity.2d.spriteshape"] = "7.0.4",
 		["com.unity.2d.tilemap.extras"] = "2.2.2",
+		["com.unity.adaptiveperformance.samsung.android"] = "4.0.0",
 		["com.unity.mobile.android-logcat"] = "1.3.2",
 		["com.unity.editorcoroutines"] = "1.0.0",
 		["com.unity.localization"] = "1.0.5",
