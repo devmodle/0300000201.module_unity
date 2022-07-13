53cf0e2e6e70b08432cafb3ca85ef110f92fb805
sd.lee <sd.lee@ninetap.com>
2022-07-13 13:12:20 +0900

    [이상동] 서브 모듈 갱신

--- 0300010101.Sample_Unity/Assets/03.SubUnityProject/Scripts/Engine/Global/Define/KDefine+EngineEnum.cs
+++ 0300010101.Sample_Unity/Assets/03.SubUnityProject/Scripts/Engine/Global/Define/KDefine+EngineEnum.cs
@@ -6,6 +6,13 @@ using UnityEngine.Events;
 
 #if EXTRA_SCRIPT_MODULE_ENABLE && ENGINE_TEMPLATES_MODULE_ENABLE
 namespace SampleEngineName {
+	/** 엔진 열거형 값 */
+	public static partial class KEngineEnumVal {
+		#region 기본
+
+		#endregion			// 기본
+	}
+
 	#region 기본
 	
 	#endregion			// 기본
