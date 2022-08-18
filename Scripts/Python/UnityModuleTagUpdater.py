b0ee53f5777a19b4a5e46ff9ef7016704ab314fa
sd.lee <sd.lee@ninetap.com>
2022-08-18 13:00:19 +0900

    [이상동] Python 스크립트 갱신

--- Scripts/Python/UnityModuleTagUpdater.py
+++ Scripts/Python/UnityModuleTagUpdater.py
@@ -5,14 +5,36 @@ oProjName = sys.argv[1]
 oTagName = sys.argv[2]
 oReplaceTagName = sys.argv[3]
 
+oCurPath = os.getcwd()
 os.system(f"python3 UnityModuleUpdater.py \"{oProjName}\"")
-os.system(f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"git tag -d \'{oTagName}\'\"")
-os.system(f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"git push origin --delete \'{oTagName}\'\"")
-os.system(f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"git tag \'{oReplaceTagName}\'\"")
-os.system(f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"git push origin --tags\"")
 
-os.chdir(f"{os.getcwd()}/../..")
-os.system(f"git tag -d \"{oTagName}\"")
-os.system(f"git push origin --delete \"{oTagName}\"")
-os.system(f"git tag \"{oReplaceTagName}\"")
-os.system(f"git push origin --tags")
+oCmdInfos = {
+	{
+		"Cmd": f"git tag -d \"{oTagName}\"",
+		"SubmoduleCmd": f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"git tag -d \'{oTagName}\'\""
+	},
+
+	{
+		"Cmd": f"git push origin --delete \"{oTagName}\"",
+		"SubmoduleCmd": f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"git push origin --delete \'{oTagName}\'\""
+	},
+
+	{
+		"Cmd": f"git tag \"{oReplaceTagName}\"",
+		"SubmoduleCmd": f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"git tag \'{oReplaceTagName}\'\""
+	},
+
+	{
+		"Cmd": f"git push origin --tags",
+		"SubmoduleCmd": f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"git push origin --tags\""
+	},
+}
+
+for oCmdInfo in oCmdInfos:
+	os.chdir(f"{oCurPath}/../..")
+
+	try:
+		os.system(oCmdInfo["Cmd"])
+	finally:
+		os.chdir(oCurPath)
+		os.system(oCmdInfo["SubmoduleCmd"])
