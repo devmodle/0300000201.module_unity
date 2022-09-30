0028b5eefd0844daae19120afb9947edd0c1d0b9
dante <lkstudio.dante@gmail.com>
2022-09-30 16:00:33 +0900

    [이상동] 엔진 버전 갱신

--- Client/0300010101.Sample_Unity/Assets/00.AutoCreate/Scripts/Editor/Global/Define/KEditorDefine+Abs.cs
+++ Client/0300010101.Sample_Unity/Assets/00.AutoCreate/Scripts/Editor/Global/Define/KEditorDefine+Abs.cs
@@ -161,7 +161,7 @@ public static partial class KEditorDefine {
 		["com.unity.mobile.android-logcat"] = "1.3.2",
 		["com.unity.editorcoroutines"] = "1.0.0",
 		["com.unity.formats.fbx"] = "4.1.2",
-		["com.unity.inputsystem"] = "1.4.2",
+		["com.unity.inputsystem"] = "1.4.3",
 		["com.unity.localization"] = "1.3.2",
 		["com.unity.performance.profile-analyzer"] = "1.1.1",
 		["com.unity.remote-config"] = "3.1.3",
