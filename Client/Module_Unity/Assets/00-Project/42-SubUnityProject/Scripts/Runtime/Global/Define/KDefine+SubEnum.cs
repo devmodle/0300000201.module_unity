50498a7d7d2c7ce534c44aebd15b02bb277c7bd3
sd.lee <sd.lee@ninetap.com>
2024-01-11 11:58:57 +0900

    [이상동] 서브 모듈 갱신

--- Client/Module_Unity/Assets/00-Project/42-SubUnityProject/Scripts/Runtime/Global/Define/KDefine+SubEnum.cs
+++ Client/Module_Unity/Assets/00-Project/42-SubUnityProject/Scripts/Runtime/Global/Define/KDefine+SubEnum.cs
@@ -86,7 +86,7 @@ public enum EApplyType {
 }
 
 /** 적용 종류 */
-public enum EApplyKinds : long {
+public enum EApplyKinds {
 	NONE = -1,
 
 	#region 다중
@@ -117,7 +117,7 @@ public enum EFXApplyType {
 }
 
 /** 효과 적용 종류 */
-public enum EFXApplyKinds : long {
+public enum EFXApplyKinds {
 	NONE = -1,
 
 	#region 애니메이션
@@ -154,7 +154,7 @@ public enum ETargetType {
 }
 
 /** 타겟 종류 */
-public enum ETargetKinds : long {
+public enum ETargetKinds {
 	NONE = -1,
 
 	#region 아이템
@@ -204,7 +204,7 @@ public enum ECalcType {
 }
 
 /** 수식 종류 */
-public enum ECalcKinds : long {
+public enum ECalcKinds {
 	NONE = -1,
 
 	#region 어빌리티
@@ -226,7 +226,7 @@ public enum EMissionType {
 }
 
 /** 미션 종류 */
-public enum EMissionKinds : long {
+public enum EMissionKinds {
 	NONE = -1,
 
 	#region 메인
@@ -265,7 +265,7 @@ public enum ERewardType {
 }
 
 /** 보상 종류 */
-public enum ERewardKinds : long {
+public enum ERewardKinds {
 	NONE = -1,
 
 	#region 무료
@@ -312,7 +312,7 @@ public enum EEpisodeType {
 }
 
 /** 에피소드 종류 */
-public enum EEpisodeKinds : long {
+public enum EEpisodeKinds {
 	NONE = -1,
 
 	#region 챕터
@@ -351,7 +351,7 @@ public enum ETutorialType {
 }
 
 /** 튜토리얼 종류 */
-public enum ETutorialKinds : long {
+public enum ETutorialKinds {
 	NONE = -1,
 
 	#region 플레이
@@ -378,7 +378,7 @@ public enum EResType {
 }
 
 /** 리소스 종류 */
-public enum EResKinds : long {
+public enum EResKinds {
 	NONE = -1,
 
 	#region 폰트
@@ -432,7 +432,7 @@ public enum EItemType {
 }
 
 /** 아이템 종류 */
-public enum EItemKinds : long {
+public enum EItemKinds {
 	NONE = -1,
 
 	#region 재화
@@ -491,7 +491,7 @@ public enum ESkillType {
 }
 
 /** 스킬 종류 */
-public enum ESkillKinds : long {
+public enum ESkillKinds {
 	NONE = -1,
 
 	#region 액션
@@ -533,7 +533,7 @@ public enum EObjType {
 }
 
 /** 객체 종류 */
-public enum EObjKinds : long {
+public enum EObjKinds {
 	NONE = -1,
 
 	#region 배경
@@ -584,7 +584,7 @@ public enum EFXType {
 }
 
 /** 효과 종류 */
-public enum EFXKinds : long {
+public enum EFXKinds {
 	NONE = -1,
 
 	#region 타격
@@ -616,7 +616,7 @@ public enum EAbilityType {
 }
 
 /** 어빌리티 종류 */
-public enum EAbilityKinds : long {
+public enum EAbilityKinds {
 	NONE = -1,
 
 	#region 스탯
@@ -771,7 +771,7 @@ public enum EProductType {
 }
 
 /** 상품 종류 */
-public enum EProductKinds : long {
+public enum EProductKinds {
 	NONE = -1,
 
 	#region 패키지
