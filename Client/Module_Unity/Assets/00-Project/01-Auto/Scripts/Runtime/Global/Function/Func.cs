e00c95cc22cb19d00a6bc46a0344c49ba3cd7c47
sd.lee <sd.lee@ninetap.com>
2024-01-29 16:54:35 +0900

    [이상동] 서브 모듈 갱신

--- Client/Module_Unity/Assets/00-Project/01-Auto/Scripts/Runtime/Global/Function/Func.cs
+++ Client/Module_Unity/Assets/00-Project/01-Auto/Scripts/Runtime/Global/Function/Func.cs
@@ -21,7 +21,7 @@ using PlayFab.SharedModels;
 using GoogleSheetsToUnity;
 #endif // #if GOOGLE_SHEET_ENABLE && (DEBUG || DEVELOPMENT_BUILD)
 
-/** 기본 함수 */
+/** 함수 */
 public static partial class Func {
 	/** 콜백 */
 	private enum ECallback {
